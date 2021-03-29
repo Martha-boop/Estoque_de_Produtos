f92554f1392b7fc823c1b6a004b3446c507d281e
Marta Neris <78042453+Martha-boop@users.noreply.github.com>
2021-03-29 13:57:37 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Estoque_de_Produtos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] produtos = new string[6]  //usando array
+            {
+               " Mesa"," cadeira"," sofá", "cama"," geladeira", "fogão"
+        };
+            Console.WriteLine(produtos[0]);
+            Console.ReadLine();
+        }
+    }
+}
